75ab27e1425efd1e86e0e9fa5f4ba047c7881ca4
Alex Chiang <alex@chizang.net>
2012-11-11 17:00:08 -0800

    Lesson 1.1: more argument parsing; introducing os and flag packages
    A small little update.
    
    -       if len(flag.Arg(0)) == 0 {
    +       if len(flag.Args()) == 0 {
    
    Above, you see me make a subtle behavior change in how I test for command
    line arguments. flag.Args() contains the non-flag command line arguments,
    and testing its length is different than testing whether we have any
    arguments at all. This type of check seems more logical to me.
    
     22         for _, arg := range flag.Args() {
    
    Above, you see how we loop over the non-flag command line arguments which
    are passed in flag.Args(). Since I don't really care about the positional
    values of the arguments, you see me using the idiomatic 'blank identifier'
    in the loop.
    
    The remainder of this commit isn't super interesting. The new functions
    introduced by the inclusion of the 'os' package have normal looking APIs.
    
     23                 ifp, err := os.Open(arg)
     24                 if err != nil {
    
    We see here that we just take the multiple return values in go functions
    for granted, getting back a file pointer in the success case and an error
    value in the error path. This is much more elegant than the C idiom:
    
    9243     if (!(ifp = fopen (ifname, "rb"))) {
    
    The above is such idiomatic C that us old crusty guys might wonder "how
    else would you do it?" but taking a step back, we see it's pretty gnarly.
    
    	a) we do variable assignment using the '=' operator inside of
    	an outer boolean test. How many zillions of bugs are out there
    	due to people screwing up an '=' vs '==' in an if clause?
    
    	b) if we have not memorized the return type of fopen(3), we must
    	assume that it is "zero-ish on failure". This is one of the worst
    	parts of C, where sometimes returning 0 means success and other
    	times, returning 0 means failure. Even the standard library is
    	inconsistent, meaning it is impossible to know for certain how to
    	test for error condition without looking at a reference.
    
    	c) and anyway, fopen(3) returns NULL in an error condition, which
    	most C programmers never think twice about, but do note that NULL
    	merits an *entire* section in the C FAQ:
    		http://c-faq.com/null/index.html
    
    	And amusingly:
    		http://c-faq.com/null/machexamp.html
    
    Ok, I guess that was more interesting than I originally supposed.
    
    Other items of note:
    
     25                         log.Fatal(err)
    
    The above does call os.Exit(1), as you might imagine.
    
     27                 buf := make([]byte, 1)
     28                 ifp.Read(buf)
    
    The above two lines are just filler here, and me starting to play around
    with actually reading from the file. They can be ignored for now.

--- dcraw.go
+++ dcraw.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"flag"
+	"log"
+	"os"
 )
 
 var identify_only bool
@@ -13,8 +15,16 @@ func init() {
 func main() {
 	flag.Parse()
 
-	if len(flag.Arg(0)) == 0 {
+	if len(flag.Args()) == 0 {
 		fmt.Println("No files to process.")
+		os.Exit(1)
 	}
-	fmt.Println(flag.Arg(0))
+	for _, arg := range flag.Args() {
+		ifp, err := os.Open(arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		buf := make([]byte, 1)
+		ifp.Read(buf)
+	}
 }
